a0f98cea4eb61766231ecf086538c5158f811b2b
maguohua <1264889788@qq.com>
2018-01-15 14:23:21 +0800

    pxq 修改接口

--- rotues/pxq.js
+++ rotues/pxq.js
@@ -1,33 +1,38 @@
 import express from 'express';
-import {applybalance, applyrecord, applysuccess, balance, policylist, products, record} from '../data/pxq';
+import {applybalance, applyrecord, applysuccess, balance, products, recordPassed, recordAudited, recordFailed} from '../data/pxq';
 const router = express.Router();
 
 router.get('/data/applybalance', (req, res, next) => {
-	res.send(applybalance)
+	res.send(applybalance);
 })
 
 router.get('/data/applyrecord', (req, res, next) => {
-	res.send(applyrecord)
+	res.send(applyrecord);
 })
 
 router.get('/data/applysuccess', (req, res, next) => {
-	res.send(applysuccess)
+	res.send(applysuccess);
 })
 
 router.get('/data/balance', (req, res, next) => {
-	res.send(balance)
-})
-
-router.get('/data/policylist', (req, res, next) => {
-	res.send(policylist)
+	res.send(balance);
 })
 
 router.get('/data/products', (req, res, next) => {
-	res.send(products)
+	res.send(products);
 })
 
-router.get('/data/record', (req, res, next) => {
-	res.send(record)
+router.get('/data/record/passed', (req, res, next) => {
+	res.send(recordPassed);
 })
+
+router.get('/data/record/audited', (req, res, next) => {
+  res.send(recordAudited);
+})
+
+router.get('/data/record/failed', (req, res, next) => {
+  res.send(recordFailed);
+})
+
 
 export default router;
